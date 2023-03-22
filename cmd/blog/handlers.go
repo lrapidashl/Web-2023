566b160da2663dccbf75f57f8509dd965746ebef
lrapidashl <odintsov_264@mail.ru>
2023-03-23 01:18:47 +0300

    Ура, бэкенд

---
+++ cmd/blog/handlers.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type indexPage struct {
+	Title         string
+	FeaturedPosts []featuredPostData
+	MostRecent    []mostRecentData
+}
+
+type featuredPostData struct {
+	Title       string
+	Subtitle    string
+	ImgModifier string
+	Author      string
+	AuthorImg   string
+	PublishDate string
+}
+
+type mostRecentData struct {
+	Title       string
+	Subtitle    string
+	Img         string
+	Author      string
+	AuthorImg   string
+	PublishDate string
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	ts, err := template.ParseFiles("pages/index.html")
+	if err != nil {
+		http.Error(w, "Internal server error", 500)
+		log.Println(err.Error())
+		return
+	}
+
+	data := indexPage{
+		Title:         "Escape.",
+		FeaturedPosts: featuredPosts(),
+		MostRecent:    mostRecent(),
+	}
+
+	err = ts.Execute(w, data) // Заставляем шаблонизатор вывести шаблон в тело ответа
+	if err != nil {
+		http.Error(w, "Internal Server Error", 500)
+		log.Println(err.Error())
+		return
+	}
+}
+
+func post(w http.ResponseWriter, r *http.Request) {
+	ts, err := template.ParseFiles("pages/post.html")
+	if err != nil {
+		http.Error(w, "Internal server error", 500)
+		log.Println(err.Error())
+		return
+	}
+
+	err = ts.Execute(w, nil) // Заставляем шаблонизатор вывести шаблон в тело ответа
+	if err != nil {
+		http.Error(w, "Internal Server Error", 500)
+		log.Println(err.Error())
+		return
+	}
+}
+
+func featuredPosts() []featuredPostData {
+	return []featuredPostData{
+		{
+			Title:       "The Road Ahead",
+			Subtitle:    "The road ahead might be paved - it might not be.",
+			ImgModifier: "featured-post_image_the-road-ahead",
+			Author:      "Mat Vogels",
+			AuthorImg:   "assets/img/Mat_Vogels.png",
+			PublishDate: "September 25, 2015",
+		},
+		{
+			Title:       "From Top Down",
+			Subtitle:    "Once a year, go someplace you’ve never been before.",
+			ImgModifier: "featured-post_image_from-top-down",
+			Author:      "William Wong",
+			AuthorImg:   "assets/img/William_Wong.png",
+			PublishDate: "September 25, 2015",
+		},
+	}
+}
+
+func mostRecent() []mostRecentData {
+	return []mostRecentData{
+		{
+			Title:       "Still Standing Tall",
+			Subtitle:    "Life begins at the end of your comfort zone.",
+			Img:         "assets/img/still_standing_tall.png",
+			Author:      "William Wong",
+			AuthorImg:   "assets/img/William_Wong.png",
+			PublishDate: "9/25/2015",
+		},
+		{
+			Title:       "Sunny Side Up",
+			Subtitle:    "No place is ever as bad as they tell you it’s going to be.",
+			Img:         "assets/img/sunny_side_up.png",
+			Author:      "Mat Vogels",
+			AuthorImg:   "assets/img/Mat_Vogels.png",
+			PublishDate: "9/25/2015",
+		},
+		{
+			Title:       "Water Falls",
+			Subtitle:    "We travel not to escape life, but for life not to escape us.",
+			Img:         "assets/img/water_falls.png",
+			Author:      "Mat Vogels",
+			AuthorImg:   "assets/img/Mat_Vogels.png",
+			PublishDate: "9/25/2015",
+		},
+		{
+			Title:       "Througt the Mist",
+			Subtitle:    "Travel makes you see what a tiny place you occupy in the world.",
+			Img:         "assets/img/through_the_mist.png",
+			Author:      "William Wong",
+			AuthorImg:   "assets/img/William_Wong.png",
+			PublishDate: "9/25/2015",
+		},
+		{
+			Title:       "Awaken Early",
+			Subtitle:    "Not all those who wander are lost.",
+			Img:         "assets/img/awaken_early.png",
+			Author:      "Mat Vogels",
+			AuthorImg:   "assets/img/Mat_Vogels.png",
+			PublishDate: "9/25/2015",
+		},
+		{
+			Title:       "Try it Always",
+			Subtitle:    "The world is a book, and those who do not travel read only one page.",
+			Img:         "assets/img/try_it_always.png",
+			Author:      "Mat Vogels",
+			AuthorImg:   "assets/img/Mat_Vogels.png",
+			PublishDate: "9/25/2015",
+		},
+	}
+}
